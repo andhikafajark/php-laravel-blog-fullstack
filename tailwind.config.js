ed452dba1fddeca577f79216d1906c66607ff298
andhikafajark <andhika.fajar2@gmail.com>
2023-04-02 13:23:33 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.{blade.php,jsx,ts,tsx,vue}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
